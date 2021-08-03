6f860c08217d2e390cc0918b723484e7b6d8fd8d
marcellodinizr <marcellodinizrocha@gmail.com>
2021-08-03 10:47:50 -0300

    <Background> , <StatusBar> and adddind fontsLoaded

--- App.tsx
+++ App.tsx
@@ -1,8 +1,38 @@
 import React from "react";
+import { useFonts } from "expo-font";
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+import {
+	Rajdhani_500Medium,
+	Rajdhani_700Bold,
+} from "@expo-google-fonts/rajdhani";
+import AppLoading from "expo-app-loading";
+
 import { SignIn } from "./src/screens/SignIn";
+import { StatusBar } from "react-native";
+import { Background } from "./src/components/Background";
 
 export default function App() {
-  return (
-    <SignIn />
-  );
+	const [fontsLoaded] = useFonts({
+		Inter_400Regular,
+		Inter_500Medium,
+		Rajdhani_500Medium,
+		Rajdhani_700Bold,
+	});
+
+	/* If the fonts aren't loaded, the splash page
+     will finish after that */
+	if (!fontsLoaded) {
+		return <AppLoading />;
+	}
+
+	return (
+		<Background>
+			<StatusBar
+				barStyle="light-content"
+				backgroundColor="transparent"
+				translucent
+			/>
+			<SignIn />
+		</Background>
+	);
 }
