4067fdf12f2ec47ad72dcce7c822feb9ec388d4b
marcellodinizr <marcellodinizrocha@gmail.com>
2021-08-03 22:25:28 -0300

    creating the handle to navigate to Home

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,33 +1,39 @@
 import React from "react";
-import { Text, View, Image } from "react-native";
+import { View, Text, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-import { ButtonIcon } from "../../components/ButtonIcon";
-import IllustratingImg from "../../assets/illustration.png";
+import IllustrationImg from "../../assets/illustration.png";
 import { styles } from "./styles";
 
+import { ButtonIcon } from "../../components/ButtonIcon";
+
 export function SignIn() {
+	const navigation = useNavigation();
+
+	function handleSignIn() {
+		navigation.navigate("Home");
+	}
+
 	return (
 		<View style={styles.container}>
-
-			
 			<Image
-				source={IllustratingImg}
+				source={IllustrationImg}
 				style={styles.image}
 				resizeMode="stretch"
 			/>
 
 			<View style={styles.content}>
 				<Text style={styles.title}>
-					Conecte-se {`\n`}e organize suas {`\n`}
+					Conecte-se {"\n"}e organize suas {"\n"}
 					jogatinas
 				</Text>
 
 				<Text style={styles.subtitle}>
-					Crie grupos para jogar seus games {`\n`}
+					Crie grupos para jogar seus games {"\n"}
 					favoritos com seus amigos
 				</Text>
 
-				<ButtonIcon title="Entrar com Discord" activeOpacity={0.7} />
+				<ButtonIcon title="Entrar com Discord" onPress={handleSignIn} />
 			</View>
 		</View>
 	);
