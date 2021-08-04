c8e8ef932d52fd70e21842efa265e2ae2cfaa20f
marcellodinizr <marcellodinizrocha@gmail.com>
2021-08-04 20:42:16 -0300

    changing TouchableOpacity to RectButton

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,11 +1,6 @@
 import React from "react";
-import {
-	Text,
-	Image,
-	View,
-	TouchableOpacity,
-	TouchableOpacityProps,
-} from "react-native";
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
+import { Text, Image, View } from "react-native";
 
 import DiscordImg from "../../assets/discord.png";
 import { styles } from "./styles";
@@ -13,18 +8,18 @@ import { styles } from "./styles";
 /* creating a type for title, if necessary
  use the Button Icon, changing the title=""
  inside SigIn index.tsx */
-type Props = TouchableOpacityProps & {
+type Props = RectButtonProps & {
 	title: string;
 };
 
 export function ButtonIcon({ title, ...rest }: Props) {
 	return (
-		<TouchableOpacity style={styles.container} {...rest}>
+		<RectButton style={styles.container} {...rest}>
 			<View style={styles.iconWrapper}>
 				<Image source={DiscordImg} style={styles.icon} />
 			</View>
 
 			<Text style={styles.title}>{title}</Text>
-		</TouchableOpacity>
+		</RectButton>
 	);
 }
