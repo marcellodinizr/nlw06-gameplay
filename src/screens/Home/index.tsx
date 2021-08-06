5e26912c91a8fdf5124a47c58f79798676a4819b
marcellodinizr <marcellodinizrocha@gmail.com>
2021-08-06 09:51:59 -0300

    general commits

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,6 +3,7 @@ import { View, FlatList } from "react-native";
 
 import { CategorySelect } from "../../components/CategorySelect";
 import { Appointment } from "../../components/Appointment";
+import { ListDivider } from "../../components/ListDivider";
 import { ListHeader } from "../../components/ListHeader";
 import { ButtonAdd } from "../../components/ButtonAdd";
 import { Profile } from "../../components/Profile";
@@ -21,11 +22,23 @@ export function Home() {
 				icon: null,
 				owner: true,
 			},
-
 			category: "1",
-			date: "22/06 ás 20:40",
+			date: "22/06 às 20:40h",
 			description:
-				"É hoje que vamos chegar ao challenger sem perder uma partida de m10",
+				"É hoje que vamos chegar ao challenger sem perder uma partida da md10",
+		},
+		{
+			id: "2",
+			guild: {
+				id: "1",
+				name: "Lendários",
+				icon: null,
+				owner: true,
+			},
+			category: "1",
+			date: "22/06 às 20:40h",
+			description:
+				"É hoje que vamos chegar ao challenger sem perder uma partida da md10",
 		},
 	];
 
@@ -40,24 +53,22 @@ export function Home() {
 				<ButtonAdd />
 			</View>
 
-			<View>
-				<CategorySelect
-					categorySelected={category}
-					setCategory={handleCategorySelect}
-				/>
+			<CategorySelect
+				categorySelected={category}
+				setCategory={handleCategorySelect}
+			/>
 
-				<View style={styles.content}>
-					<ListHeader title="Partidas agendadas" subtitle="Total 6" />
+			<View style={styles.content}>
+				<ListHeader title="Partidas agendadas" subtitle="Total 6" />
 
-					<FlatList
-						data={appointments}
-						keyExtractor={(item) => item.id}
-						renderItem={({ item }) => <Appointment data={item} />}
+				<FlatList
+					data={appointments}
+					keyExtractor={(item) => item.id}
+					renderItem={({ item }) => <Appointment data={item} />}
 					ItemSeparatorComponent={() => <ListDivider />}
 					style={styles.matches}
 					showsVerticalScrollIndicator={false}
-					/>
-				</View>
+				/>
 			</View>
 		</View>
 	);
