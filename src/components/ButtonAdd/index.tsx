a0b2417681c450b5e31ae8932112ec7bef2de4a5
marcellodinizr <marcellodinizrocha@gmail.com>
2021-08-11 10:46:09 -0300

    forgot to add {...rest}

--- src/components/ButtonAdd/index.tsx
+++ src/components/ButtonAdd/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { View } from "react-native";
 import { styles } from "./styles";
 import { RectButton, RectButtonProps } from "react-native-gesture-handler";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
@@ -7,7 +6,7 @@ import { theme } from "../../global/styles/theme";
 
 export function ButtonAdd({ ...rest }: RectButtonProps) {
 	return (
-		<RectButton style={styles.container}>
+		<RectButton style={styles.container} {...rest}>
 			<MaterialCommunityIcons
 				name="plus"
 				color={theme.colors.heading}
