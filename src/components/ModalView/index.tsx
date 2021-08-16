6da50b65918707f9e68f4f61014a6e3cbbb08486
marcellodinizr <marcellodinizrocha@gmail.com>
2021-08-16 10:21:42 -0300

    adding TouchableWithoutFeedback

--- src/components/ModalView/index.tsx
+++ src/components/ModalView/index.tsx
@@ -1,5 +1,10 @@
 import React, { ReactNode } from "react";
-import { View, Modal, ModalProps } from "react-native";
+import {
+	View,
+	Modal,
+	ModalProps,
+	TouchableWithoutFeedback,
+} from "react-native";
 import { Background } from "../Background";
 
 import { styles } from "./styles";
@@ -10,15 +15,17 @@ type Props = ModalProps & {
 
 export function ModalView({ children, ...rest }: Props) {
 	return (
-		<Modal transparent animationType="slide" {...rest}>
-			<View style={styles.overlay}>
-				<View style={styles.container}>
-					<Background>
-						<View style={styles.bar} />
-						{children}
-					</Background>
+		<Modal transparent animationType="slide" statusBarTranslucent {...rest}>
+			<TouchableWithoutFeedback onPress={closeModal}>
+				<View style={styles.overlay}>
+					<View style={styles.container}>
+						<Background>
+							<View style={styles.bar} />
+							{children}
+						</Background>
+					</View>
 				</View>
-			</View>
+			</TouchableWithoutFeedback>
 		</Modal>
 	);
 }
