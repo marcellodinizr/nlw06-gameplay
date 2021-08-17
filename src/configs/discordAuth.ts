e8606a8a14b4d184a926f49d1cb0699c181709fa
marcellodinizr <marcellodinizrocha@gmail.com>
2021-08-17 11:46:27 -0300

    creating the discord auth

---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,7 @@
+const REDIRECT_URI = "https%3A%2F%2Fauth.io%2Fgameplay";
+const SCOPE = "identify%20email%20connections%20guilds";
+const RESPONSE_TYPE = "token";
+const CLIENT_ID = "877171874688303125";
+const CDN_IMAGE = "https://cdn.discrodapp.com";
+
+export { REDIRECT_URI, SCOPE, RESPONSE_TYPE, CLIENT_ID, CDN_IMAGE };
