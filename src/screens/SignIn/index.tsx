f9f5a8e58150a48f6b8dc46bb1075d90bc0fe743
marcellodinizr <marcellodinizrocha@gmail.com>
2021-08-17 11:47:02 -0300

    adding the useAuth in the handleSignIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { View, Text, Image, Alert } from "react-native";
+
+import { useAuth } from "../../hooks/auth";
 
 import IllustrationImg from "../../assets/illustration.png";
 import { styles } from "./styles";
@@ -9,10 +10,14 @@ import { ButtonIcon } from "../../components/ButtonIcon";
 import { Background } from "../../components/Background";
 
 export function SignIn() {
-	const navigation = useNavigation();
+	const { user, signIn } = useAuth();
 
-	function handleSignIn() {
-		navigation.navigate("Home");
+	async function handleSignIn() {
+		try {
+			await signIn();
+		} catch (error) {
+			Alert.alert(error);
+		}
 	}
 
 	return (
